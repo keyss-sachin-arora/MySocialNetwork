7e85f268dc4a91a42d25e95890b9dc80af96ea1e
Gagandeep Singh <gagandeep@signitysolutions.com>
2016-12-12 14:49:45 +0530

    complete firebase setup

---
+++ SocialNetwork/SigninVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Mac Mini on 12/12/16.
+//  Copyright © 2016 Mac Mini. All rights reserved.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
